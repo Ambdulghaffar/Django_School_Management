cb9e7d4c99b22d05558666e420cf903272b4026b
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-04-12 21:45:37 +0100

    Home page commit

--- School_Management/settings.py
+++ School_Management/settings.py
@@ -118,10 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+""" STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-]
+] """
+
+STATIC_URL = '/static/'
+
+# Si tu es en mode développement :
+STATICFILES_DIRS = [BASE_DIR / "static"]  # facultatif si tu veux centraliser
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
